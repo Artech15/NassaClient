ee2232349c236bf3509af852c92b9c8845f5f0af
arlinka-a <arlin.agarunov@gmail.com>
2023-10-30 09:15:57 +0200

    not finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
-import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
-import Temp from './pages/Temp/Temp';
-import { Group } from './pages/Group/Group';
-import './app.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import Temp from "./pages/Temp/Temp";
+import { Group } from "./pages/Group/Group";
+import "./app.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route index element={<Login />} /> */}
-        <Route index element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/forgotPassword' element={<ForgotPassword />} />
-        <Route path='/group' element={<Group />} />
-        <Route path='/temp' element={<Temp />} />
+        <Route index element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route index element={<Home />} /> */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/group" element={<Group />} />
+        <Route path="/temp" element={<Temp />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,74 +1,119 @@
-import React, { useEffect, useState } from 'react';
-import { Container, StyledTop, FlexContainer, Title, Text, StyledInput, StyledBottom, CourseContainer,MyCoursesTitle, GroupsContainer } from './styles';
-import httpCommon from '../../services/http-common';
-import CourseItem from '../../components/CourseItem/CourseItem';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  StyledTop,
+  FlexContainer,
+  Title,
+  Text,
+  StyledInput,
+  StyledBottom,
+  CourseContainer,
+  MyCoursesTitle,
+  GroupsContainer,
+} from "./styles";
+import httpCommon from "../../services/http-common";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import CourseItem from "../../components/CourseItem/CourseItem";
+import IconButton from "@mui/material/IconButton";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Home() {
-    const user = JSON.parse(localStorage.getItem('user'));
-    const [groups, setGroups] = useState([])
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [groups, setGroups] = useState([]);
+  const [filter, setFilter] = useState();
 
-    const getUserCourse = async (groupId) => {
-        try {
-            const res = await httpCommon.get(`/group/${groupId}`);
-            // setGroups((prevGroup) => [...prevGroup, res.data]);
-            if(res.data.type === 'course'){
-                return res.data
-            }
-            return null
+  const itemsToFilter = [
+    { name: "קורס יסוד", searchTag: "קורס יסוד" },
+    { name: "קורס מתקדם", searchTag: "קורס מתקדם" },
+    { name: "השתלמות מקצועית", searchTag: "השתלמות מקצועית" },
+    { name: "הכשרה פיקודית", searchTag: "הכשרה פיקודית" },
+  ];
 
-        } catch (error) {
-            console.error('Group get error:', error);
-        }
+  const getUserCourse = async (groupId) => {
+    try {
+      const res = await httpCommon.get(`/group/${groupId}`);
+      // setGroups((prevGroup) => [...prevGroup, res.data]);
+      if (res.data.type === "course") {
+        return res.data;
+      }
+      return null;
+    } catch (error) {
+      console.error("Group get error:", error);
     }
+  };
 
   useEffect(() => {
     // Map user.groups to an array of promises
-    const promiseGroups = user.groups.map(group => getUserCourse(group));
+    const promiseGroups = user?.groups.map((group) => getUserCourse(group));
 
     // Use Promise.all to wait for all promises to resolve
     Promise.all(promiseGroups)
-      .then(dataArray => {
+      .then((dataArray) => {
         setGroups(dataArray);
       })
-      .catch(error => {
-        console.error('Error fetching groups:', error);
+      .catch((error) => {
+        console.error("Error fetching groups:", error);
       });
   }, []);
 
-    return(
-        <Container>
-            <StyledTop>
-                <FlexContainer>
-                    <Title>NASSA</Title>
-                    <Text>מערכת לניהול קורסים ותיקי חניכים</Text>
-                    {/* <StyledInput></StyledInput> */}
-                     <StyledInput
-                        placeholder="חיפוש אנשים וקורסים באמ״ן"
-                        // variant="outlined"/
-                        // fullWidth
-                        onChange={() => console.log('dfsc')}
-                        InputProps={{
-                          startAdornment: (
-                        <IconButton edge="start" aria-label="search" style={{color:'rgb(218,218,218)', transform: 'scaleX(-1)'}}>
-                              <SearchIcon style={{fontSize: '2rem'}} />
-                            </IconButton>
-                          ),
-                        }}
-                     />
-                </FlexContainer>
-            </StyledTop>
-            <StyledBottom>
-                <CourseContainer>
-                    <MyCoursesTitle>הקורסים שלי</MyCoursesTitle>
-                    <GroupsContainer>
-                        {groups.map(group => {
-                            return group !== null ? <CourseItem group={group}/> : null
-                        })}
-                    </GroupsContainer>
-                </CourseContainer>
-            </StyledBottom>
-        </Container>
-    )
+  return (
+    <Container>
+      <StyledTop>
+        <FlexContainer>
+          <Title>NASSA</Title>
+          <Text>מערכת לניהול קורסים ותיקי חניכים</Text>
+          {/* <StyledInput></StyledInput> */}
+          <StyledInput
+            placeholder="חיפוש אנשים וקורסים באמ״ן"
+            // variant="outlined"/
+            // fullWidth
+            onChange={(event) => console.log("event.target.value")}
+            InputProps={{
+              startAdornment: (
+                <IconButton
+                  edge="start"
+                  aria-label="search"
+                  style={{ color: "rgb(218,218,218)", transform: "scaleX(-1)" }}
+                >
+                  <SearchIcon style={{ fontSize: "2rem" }} />
+                </IconButton>
+              ),
+            }}
+          />
+        </FlexContainer>
+      </StyledTop>
+      <StyledBottom>
+        <CourseContainer>
+          <MyCoursesTitle>הקורסים שלי</MyCoursesTitle>
+          <FormControl sx={{ m: 4, minWidth: 90 }}>
+            <InputLabel id="demo-simple-select-label">סוג קורס</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={filter}
+              label="filter"
+              onChange={(e) => {
+                console.log(e.target);
+              }}
+            >
+              <MenuItem value="">
+                <em>הכל</em>
+              </MenuItem>
+              {itemsToFilter.map((item) => (
+                <MenuItem value={item.name}>{item.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <GroupsContainer>
+            {groups.map((group) => {
+              return group !== null ? <CourseItem group={group} /> : null;
+            })}
+          </GroupsContainer>
+        </CourseContainer>
+      </StyledBottom>
+    </Container>
+  );
 }
